7c406dd1854d07426463b3f91463b0658f539492
Lawrence <53685470+lawrence-cs@users.noreply.github.com>
2020-08-10 18:30:03 +1000

    added login buttons and functions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+firebase.initializeApp({
+  apiKey: "AIzaSyBYYhmwVTO2rPqAsZbQrMsk2jrBBVZOlLw",
+  authDomain: "react-firebase-login-db.firebaseapp.com"
+})
+
+class App extends React.Component {
+  state = {
+    isSignedIn: false
+  }
+
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callback: {
+      signInSuccess: () => false
+    }
+  }
+
+ 
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({isSignedIn:!!user}) //!!user means that if user isSignedIn covert it to !isSigneIn
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        {
+          this.state.isSignedIn ? (
+            <Fragment>
+              <div>
+                <h1>Hello {firebase.auth().currentUser.displayName}</h1>
+                <img alt="profile picture" src={firebase.auth().currentUser.photoURL} />
+              </div>
+              <div>
+                <button onClick={()=>firebase.auth().signOut()}>Sign Out</button>
+              </div>
+            </Fragment>
+          ) : (
+            <StyledFirebaseAuth
+              uiConfig={this.uiConfig}
+              firebaseAuth={firebase.auth()}
+            />
+          )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
